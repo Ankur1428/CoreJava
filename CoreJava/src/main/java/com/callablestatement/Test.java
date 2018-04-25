bcc46fa0aefafc9aba9c2afa4dfc4b057182f9df
Ankur Aherkar <ankuraherkar.com>
2018-04-25 12:25:05 +0530

    first Change

--- CoreJava/src/main/java/com/callablestatement/Test.java
+++ CoreJava/src/main/java/com/callablestatement/Test.java
@@ -2,6 +2,20 @@ package com.callablestatement;
 
 public class Test {
 public static void main(String[] args) {
+	System.out.println("hello");
+	m1();
+	newMethod();
 	
+}
+
+private static void newMethod() {
+	// TODO Auto-generated method stub
+	System.out.println("new mmethod"); 
+	
+}
+
+private static void m1() {
+	// TODO Auto-generated method stub
+	System.out.println("inside m1");
 }
 }
